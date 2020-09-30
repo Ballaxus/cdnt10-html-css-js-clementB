ad4db1e371b392cc71b53065427ebf8161f4a175
Ballaxus <clementballeur@gmail.com>
2020-09-30 15:23:13 +0000

    exo JS JQ JQ CDNT10

---
+++ TP/tp-js-j3/script.js
@@ -0,0 +1,16 @@
+// var code1 = $("#code1input");
+// console.log(code1);
+
+// var btnValidate = $("#btn-validate");
+
+// btnValidate.attr('disabled', true);
+// console.log(btnValidate.val);
+// code1.keyup(() => {
+//   // var disabled = txtUsername.val().length < 3;
+//   if(code1.val().length =8) {
+
+//     btnValidate.attr('disabled', false);
+//   }
+//   else
+//   btnValidate.attr('disabled', true);
+// })
